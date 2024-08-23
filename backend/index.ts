769ef0a1dd98730b58a35a09617a76d4d6d81f0f
manish vishwakarma <prakashvish1973@gmail.com>
2024-08-23 20:37:17 +0530

    undo

--- backend/index.ts
+++ backend/index.ts
@@ -1,33 +1,10 @@
 import express from "express";
 import type { Request,Response } from "express";
-// import "./config/db.js";
+import "./config/db.js";
 import schoolRoute from "./routes/school.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001
-
-import mysql from "mysql2";
-import "dotenv/config";
-
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.DATABASE
-})
-
-console.log(process.env.HOST,process.env.user,process.env.password,process.env.DATABASE)
-
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err.message);
-    } else {
-        console.log('Connected to MySQL database');
-    }
-});
-
-export {db}
-
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
